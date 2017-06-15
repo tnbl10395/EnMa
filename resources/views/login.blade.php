cf35b5a3692a89454fe87d64fd2477bce95eeb45
n3vjppro <n3vjppro@gmail.com>
2017-06-15 16:45:32 +0700

    Ver 2.0

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,17 +4,17 @@
 <head>
         <title>Matrix Admin</title><meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="css/bootstrap.min.css" />
-		<link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
-        <link rel="stylesheet" href="css/matrix-login.css" />
-        <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+		<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+		<link rel="stylesheet" href="{{asset('css/bootstrap-responsive.min.css')}}" />
+        <link rel="stylesheet" href="{{asset('css/matrix-login.css')}}" />
+        <link href="{{asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 
     </head>
     <body>
         <div id="loginbox">            
-            <form id="loginform" class="form-vertical" action="index.html">
-				 <div class="control-group normal_text"> <h3><img src="img/logo.png" alt="Logo" /></h3></div>
+            <form id="loginform" class="form-vertical" action="index">
+				 <div class="control-group normal_text"> <h3><img src="{{asset('img/logo.png')}}" alt="Logo" /></h3></div>
                 <div class="control-group">
                     <div class="controls">
                         <div class="main_input_box">
@@ -31,7 +31,7 @@
                 </div>
                 <div class="form-actions">
                     <span class="pull-left"><a href="#" class="flip-link btn btn-info" id="to-recover">Lost password?</a></span>
-                    <span class="pull-right"><a type="submit" href="index.html" class="btn btn-success" /> Login</a></span>
+                    <span class="pull-right"><a type="submit" href="/" class="btn btn-success" /> Login</a></span>
                 </div>
             </form>
             <form id="recoverform" action="#" class="form-vertical">
@@ -45,13 +45,13 @@
                
                 <div class="form-actions">
                     <span class="pull-left"><a href="#" class="flip-link btn btn-success" id="to-login">&laquo; Back to login</a></span>
-                    <span class="pull-right"><a class="btn btn-info"/>Reecover</a></span>
+                    <span class="pull-right"><a class="btn btn-info"/>Recover</a></span>
                 </div>
             </form>
         </div>
-        
-        <script src="js/jquery.min.js"></script>  
-        <script src="js/matrix.login.js"></script> 
+
+        <script src="../../public/js/jquery.min.js"></script>
+        <script src="../../public/js/matrix.login.js"></script>
     </body>
 
 </html>
