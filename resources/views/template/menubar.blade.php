023d0af92137a45964bfdabd092507fe6ee99fa7
root <tnbaolongit@gmail.com>
2017-06-30 10:34:38 +0700

    Update menubar

--- resources/views/template/menubar.blade.php
+++ resources/views/template/menubar.blade.php
@@ -93,7 +93,7 @@
     </li>
     
     <li><a href="login"><i class="icon icon-fullscreen"></i> <span>Log out</span></a></li>
-<!-- 
+
     <li class="content"> <span>Monthly Bandwidth Transfer</span>
       <div class="progress progress-mini progress-danger active progress-striped">
         <div style="width: 77%;" class="bar"></div>
@@ -107,7 +107,7 @@
       </div>
       <span class="percent">87%</span>
       <div class="stat">604.44 / 4000 MB</div>
-    </li> -->
+    </li>
 
   </ul>
 </div>
