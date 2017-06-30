c35db8c0ea9b902303361f602a60b93a23e57cc3
root <tnbaolongit@gmail.com>
2017-06-30 10:50:55 +0700

    Thu thoi ma

--- resources/views/template/menubar.blade.php
+++ resources/views/template/menubar.blade.php
@@ -93,7 +93,7 @@
     </li>
     
     <li><a href="login"><i class="icon icon-fullscreen"></i> <span>Log out</span></a></li>
-
+      <!-- ahihi -->
 <!--     <li class="content"> <span>Monthly Bandwidth Transfer</span>
       <div class="progress progress-mini progress-danger active progress-striped">
         <div style="width: 77%;" class="bar"></div>
