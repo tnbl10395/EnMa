3f0f16101f467025578780cb8ab5c30369f8e1da
n3vjppro <n3vjppro@gmail.com>
2017-07-17 17:30:21 +0700

    Update send email

--- app/Mail/SendBirthdayMail.php
+++ app/Mail/SendBirthdayMail.php
@@ -1,30 +1,31 @@
 <?php
 
 namespace App\Mail;
-
+use Carbon\Carbon;
+use App\Engineer;
+use App\Http\Controllers\HomeController;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendMail extends Mailable
+class SendBirthdayMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-
+    public $engineer;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Engineer $engineer)
     {
+        $this->engineer=$engineer->select('engineerName')->where('status',1)->where('birthday_mail',0)
+            ->whereDay('birthday', Carbon::NOW()->day)
+            ->whereMonth('birthday', Carbon::NOW()->month)
+            ->get();;
 
     }
 
@@ -38,9 +39,6 @@ class SendMail extends Mailable
     {
         $this->from("agent.enclave@gmail.com")->view('email.birthday');
 
-        $this->withSwiftMessage(function ($message) {
-            $message->getHeaders()
-                ->addTextHeader('Custom-Header', 'HeaderValue');
-        });
+
     }
 }
