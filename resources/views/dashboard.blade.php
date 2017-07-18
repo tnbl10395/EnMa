6b684c41344e528d8877c3ae7da6df5b46d9a997
tnbl10395 <tnbaolongit@gmail.com>
2017-07-18 12:50:46 +0700

    Leesin ad (#51)
    * Leesin is AD carry
    
    * LeesinAD 14-Jul
    
    * Leesin 14-Jul 2
    
    * Save at the moment
    
    * Update the end of Friday
    
    * Update by Leesin 17-Jul
    
    * Update by LeesinAD
    
    * Update by Leesin 18-Jul v2
    
    * Update by LeesinAD Jul-18 v3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -171,7 +171,7 @@
 	          </div>
 	        </div>
 	     </div>   
-        <!-- end-new project -->
+        <!-- end-new project <--></-->
 		
 	</div>
 	</div>
@@ -181,14 +181,12 @@
             <h3></h3>
         </div>
         <div class="modal-body">
-       		<h4></h4>
         </div>
   </div>
 	@if((isset($birthday[0])!=NULL)||(isset($newEngineerNoti[0])!=NULL)||(isset($newProjectNoti[0])!=NULL)||(isset($newTeamNoti[0])!=NULL)))
 		<link rel="stylesheet" href="{{asset('css/jquery.gritter.css')}}"/>  
 	    <script src="{{asset('js/jquery.peity.min.js')}}"></script>
 	    <script src="{{asset('js/jquery.gritter.min.js')}}"></script>
-	    <!-- <script type="text/javascript" src="{{asset('js/myjs.php')}}"></script> -->
 		<script type="text/javascript">
 			birthday = "{{ $birthday }}";console.log(birthday);
 			newEngi = "{{ $newEngineerNoti }}";console.log(newEngi);
@@ -196,85 +194,5 @@
 			newTeam = "{{ $newTeamNoti }}";console.log(newTeam);
 		</script>
 		<script src='{{asset("js/matrix.interface.js")}}'></script> 
-<!-- 		<script type="text/javascript">
-
-		$(document).ready(function(){
-			birthday = "{{ $birthday }}";console.log(birthday);
-			newEngi = "{{ $newEngineerNoti }}";console.log(newEngi);
-			newPro = "{{ $newProjectNoti }}";console.log(newPro);
-			newTeam = "{{ $newTeamNoti }}";console.log(newTeam);
-			if((birthday!="[]")||(newEngi!="[]")||(newPro!="[]")||(newTeam!="[]")){
-				$.gritter.add({
-
-					title:	'Notification!',
-					text:	'You have a new notification.',
-					image: 	'img/demo/envelope.png',
-					sticky: false,		
-				});
-				$(".gritter-item").mousemove(function() {
-					$(".gritter-title").css("margin-left","-50px");	
-					$(".gritter-image").css('display', 'none');
-					$(".gritter-item p").css('display', 'none');
-					String1 = "<a href='#myModal' id='birthday' data-toggle='modal'><i class='icon-gift'></i>Today is the birthday of Engineers!</a>";
-					String2 = "<a href='#myModal' id='newEngi' data-toggle='modal'><i class='icon-user'></i>The company has some new Employees!</a>";
-					String3 = "<a href='#myModal' id='newPro' data-toggle='modal'><i class='icon-file'></i>The company has some new Projects!</a>";
-					String4 = "<a href='#myModal' id='newTeam' data-toggle='modal'><i class='icon-group'></i>The company has some new Teams!</a>";
-					// if((birthday!=null)||(newEngi!=null)){
-						$(".gritter-title").html("");
-						if (birthday!="[]") {
-							$(".gritter-title").append(String1+"<br/>");
-							$("#birthday").click(function(){
-								$(".modal-header h3").html("Happy birthday!");
-								$(".modal-body h4").html("@foreach ($birthday as $engineer)"
-													+"{{$engineer->engineerName}}"
-													+"<hr/>"
-													+"@endforeach");
-							});
-						}
-						if (newEngi!="[]") {
-							$(".gritter-title").append(String2+"<br/>");
-							$("#newEngi").click(function(){
-								$(".modal-header h3").html("New Engineers!");
-								$(".modal-body h4").html("@foreach ($newEngineerNoti as $engineer)"
-													+"{{$engineer->engineerName}}"
-													+"<hr/>"
-													+"@endforeach");
-							});
-						}
-						if (newPro!="[]") {
-							$(".gritter-title").append(String3+"<br/>");
-							$("#newPro").click(function(){
-								$(".modal-header h3").html("New Projects!");
-								$(".modal-body h4").html("<table class='table table-hover' style='border-style:hidden;'>"
-													+"@foreach ($newProjectNoti as $project)"
-													+"<tr class='gradeX'>"
-													+"<td>{{$project->idProject}}</td>"
-													+"<td>{{$project->projectName}}</td>"
-													+"<td>{{$project->dateOfBegin}}</td>"
-													+"</tr>"
-													+"@endforeach"
-													+"</table>");
-							});
-						}
-						if (newTeam!="[]") {
-							$(".gritter-title").append(String4+"<br/>");
-							$("#newTeam").click(function(){
-								$(".modal-header h3").html("New Teams!");
-								$(".modal-body h4").html("<table class='table table-hover' style='border-style:hidden;'>"
-													+"@foreach ($newTeamNoti as $team)"
-													+"<tr class='gradeX'>"
-													+"<td>{{$team->idTeam}}</td>"
-													+"<td>{{$team->teamName}}</td>"
-													+"<td>{{$team->status}}</td>"
-													+"</tr>"
-													+"@endforeach"
-													+"</table>");
-							});
-						}
-					// }
-				});
-			}
-		});
-		</script> -->
 	@endif
 @stop
