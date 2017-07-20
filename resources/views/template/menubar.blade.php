6f9d6619f2b4f085c80c66b2ccd99afb05192288
Ngô Ngọc Nhân <n3vjppro@gmail.com>
2017-07-20 13:50:57 +0700

    Nathan (#66)
    * Config & prepair for email
    
    * config mail
    
    * Update send email
    
    * Complete Send Birthday mail
    
    * Prepair sending assign mail
    
    * Nathan 15:47
    
    * Nathan 15:48
    
    * delete google js
    
    * Send assign mail

--- resources/views/template/menubar.blade.php
+++ resources/views/template/menubar.blade.php
@@ -19,7 +19,9 @@
     <link rel="stylesheet" href="{{asset('css/mycss.css')}}" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-    <script src="{{asset('js/jquery-1.12.4.js')}}"></script>
+
+ <script src="{{asset('js/jquery-1.12.4.js')}}"></script>
+
     <script src="{{asset('js/jquery.min.js')}}"></script>
     <script src="{{asset('js/jquery.ui.custom.js')}}"></script>
     <script src="{{asset('js/matrix.tables.js')}}" sycn></script>
@@ -30,7 +32,10 @@
     <script src="{{asset('js/jquery.uniform.js')}}"></script>
     <script src="{{asset('js/select2.min.js')}}"></script>
     <script src="{{asset('js/matrix.js')}}"></script>
-    <!-- <script src="{{asset('js/matrix.form_common.js')}}"></script> -->
+
+
+    {{--<script src="{{asset('js/matrix.form_common.js')}}"></script>--}}
+
     <script src="{{asset('js/bootstrap-colorpicker.js')}}"></script>
     <script src="{{asset('js/bootstrap-datepicker.js')}}"></script>
     <script src="{{asset('js/jquery.toggle.buttons.js')}}"></script> 
